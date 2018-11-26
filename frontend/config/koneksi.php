c1d5fa2e76e85a54186e375e4b8d9477700f3204
kresno <kresno.19@gmail.com>
2018-11-26 14:11:24 +0700

    edit 9

--- frontend/config/koneksi.php
+++ frontend/config/koneksi.php
@@ -1,12 +1,12 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "simencrang_kegiatan";
+// $username = "root";
+// $password = "";
+// $dbname = "simencrang_kegiatan";
 
-// $username = "simencrang2018_kegiatan";
-// $password = "pass8080word";
-// $dbname = "simencrang2018_kegiatan";
+$username = "simencrang2018_kegiatan";
+$password = "pass8080word";
+$dbname = "simencrang2018_kegiatan";
 
 
 
