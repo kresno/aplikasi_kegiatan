f6892e9b02c2662c3577c316fee8ddc1339cd3bf
kresno <kresno.19@gmail.com>
2018-11-26 09:20:47 +0700

    edit 8

--- frontend/config/koneksi.php
+++ frontend/config/koneksi.php
@@ -1,12 +1,13 @@
 <?php
 $servername = "localhost";
-// $username = "ruangaci";
-// $password = "085310526076atisser221292";
-// $dbname = "ruangaci_pj";
+// $username = "root";
+// $password = "";
+// $dbname = "simencrang_kegiatan";
 
 $username = "simencrang2018_kegiatan";
 $password = "pass8080word";
 $dbname = "simencrang2018_kegiatan";
+
 
 
 // Create connection
