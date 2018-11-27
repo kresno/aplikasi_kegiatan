76896c712cb5ace789554e79e1ae66dca39ba996
kresno <kresno.19@gmail.com>
2018-11-27 23:00:07 +0700

    add page

---
+++ admin/index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+    include '../config/head.php';
+?>
+
+<body class="fix-header">
+    <!-- ============================================================== -->
+    <!-- Preloader -->
+    <!-- ============================================================== -->
+    <div class="preloader">
+        <svg class="circular" viewBox="25 25 50 50">
+            <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" />
+        </svg>
+    </div>
+    <!-- ============================================================== -->
+    <!-- Wrapper -->
+    <!-- ============================================================== -->
+    <div id="wrapper">
+        <!-- ============================================================== -->
+        <?php
+        include '../config/navbar.php';
+        ?>
+        <!-- ============================================================== -->
+        <!-- Left Sidebar - style you can find in sidebar.scss  -->
+        <!-- ============================================================== -->    
+        <?php 
+            include '../config/sidebar.php';
+        ?>
+        <!-- ============================================================== -->
+        <!-- End Left Sidebar -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Page Content -->
+        <!-- ============================================================== -->
+        <div id="page-wrapper">
+            <div class="container-fluid">
+                <div class="row bg-title">
+                    <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+                        <h4 class="page-title">Dashboard</h4> 
+                    </div>
+                    <!-- /.col-lg-12 -->
+                </div>
+                <!-- /.row -->
+                <!-- ============================================================== -->
+                <!-- Different data widgets -->
+                <!-- ============================================================== -->
+                <!-- .row -->
+                <div class="row">
+                    <div class="col-lg-4 col-sm-6 col-xs-12">
+                        <div class="white-box analytics-info">
+                            <h3 class="box-title">Total Program</h3>
+                            <ul class="list-inline two-part">
+                                <li>
+                                    <div id="sparklinedash"></div>
+                                </li>
+                                <li class="text-right"><i class="ti-arrow-up text-success"></i> <span class="counter text-success"><?php if($jumlah_program>0) {echo $jumlah_program;} else { echo "0";} ?></span></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-sm-6 col-xs-12">
+                        <div class="white-box analytics-info">
+                            <h3 class="box-title">Total Kegiatan</h3>
+                            <ul class="list-inline two-part">
+                                <li>
+                                    <div id="sparklinedash2"></div>
+                                </li>
+                                <li class="text-right"><i class="ti-arrow-up text-purple"></i> <span class="counter text-purple"><?php if($jumlah_kegiatan>0) {echo $jumlah_kegiatan;} else { echo "0";} ?></span></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-sm-6 col-xs-12">
+                        <div class="white-box analytics-info">
+                            <h3 class="box-title">Total Indikator</h3>
+                            <ul class="list-inline two-part">
+                                <li>
+                                    <div id="sparklinedash3"></div>
+                                </li>
+                                <li class="text-right"><i class="ti-arrow-up text-info"></i> <span class="counter text-info"><?php if($jumlah_indikator>0) {echo $jumlah_indikator;} else { echo "0";} ?></span></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row bg-title">
+                    <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+                        <h4 class="page-title">Agenda Perencanaan</h4> 
+                    </div>
+                    <!-- /.col-lg-12 -->
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                    <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=le7ejga7vslrrs3cs5jgqct2o8%40group.calendar.google.com&amp;color=%232F6309&amp;ctz=Asia%2FJakarta" style="border-width:0" width="100%" height="600" frameborder="0" scrolling="no"></iframe>
+                    </div>
+                </div>
+                <!--/.row -->
+                <!--row -->
+                <!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+            <?php include '../config/footer.php';?>
+</body>
+
+</html>
--- process_login.php
+++ process_login.php
@@ -14,11 +14,18 @@ $sql="SELECT * FROM users where username='$email' and password='$password_hash' 
 if($result = mysqli_query($con, $sql)){
     if(mysqli_num_rows($result) > 0){
         $row = mysqli_fetch_assoc($result);
-        session_start();
-        $_SESSION['username'] = $row['username'];
-        $_SESSION['opd_id'] = $row['opd_id'];
         
-        echo "<script>alert('Login Berhasil'); window.location.href='frontend/index.php';</script>";
+        if($row['level_id']==2){
+            session_start();
+            $_SESSION['username'] = $row['username'];
+            $_SESSION['opd_id'] = $row['opd_id'];
+            echo "<script>alert('Login Berhasil'); window.location.href='frontend/index.php';</script>";
+        } else if($row['level_id']==3){
+            $_SESSION['username'] = $row['username'];
+            $_SESSION['bidang_id'] = $row['bidang_id'];
+            echo "<script>alert('Login Berhasil'); window.location.href='admin/index.php';</script>";
+        }
+        
     }
     else{
         echo "<script>alert('Username dan Password tidak ditemukan'); window.location.href='index.php';</script>";
