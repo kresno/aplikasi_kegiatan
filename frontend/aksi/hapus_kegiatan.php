de98a884dfd9e91d2dc74d3240f3236a780a3e33
kresno <kresno.19@gmail.com>
2018-11-27 23:35:13 +0700

    add page

--- frontend/aksi/hapus_kegiatan.php
+++ frontend/aksi/hapus_kegiatan.php
@@ -3,7 +3,7 @@ include '../config/koneksi.php';
 
 $kegiatan_id = $_GET['kegiatan_id'];
 
-$sql = "DELETE FROM kegiatan where id=$kegiatan_id; DELETE FROM indikator_kegiatan where kegiatan_id=$kegiatan_id; DELETE FROM opd_kegiatan where kegiatan_id=$kegiatan_id;";
+$sql = "DELETE FROM kegiatan where id=$kegiatan_id; DELETE FROM indikator_kegiatan where kegiatan_id=$kegiatan_id; DELETE FROM opd_kegiatan where kegiatan_id=$kegiatan_id; DELETE FROM dokumen where kegiatan_id=$kegiatan_id;";
 
 if (!mysqli_multi_query($con,$sql)) {
     die('Error: ' . mysqli_error($con));
